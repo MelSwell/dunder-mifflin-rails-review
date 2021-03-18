54b98880881c0065dac3ced2a12bed0bd5e31b91
Henry <hvelas93@gmail.com>
2021-03-18 17:24:42 -0400

    2nd swap

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -6,4 +6,15 @@ class EmployeesController < ApplicationController
   def show
     @employee = Employee.find(params[:id])
   end
+
+  def create
+  @x = Employee.create(params.require(:employee).permit(:first_name, :last_name, :alias, :title, :office, :img_url))
+  
+  redirect_to employee_path(@x)
+  end
+
+  def new
+    @new_employee=Employee.new
+  end 
+
 end
